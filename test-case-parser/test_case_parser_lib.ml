3a047ae52544d942023cd6cc168d84e9e622aa12
Arnaud Bihan <arnaud.bihan@functori.com>
2026-08-18 14:01:28 +0200

    Print

--- test-case-parser/test_case_parser_lib.ml
+++ test-case-parser/test_case_parser_lib.ml
@@ -1955,6 +1955,7 @@ let run_test include_dirs options testing_scope =
           testing_scope msg;
         [])
   in
+  Format.printf "Bonjour@.";
   let test_run = { O.test; O.assert_failures; O.diffs; O.variable_failures } in
   write_stdout J.write_test_run test_run
 
